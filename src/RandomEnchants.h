c3a26ede3ee6f4ca99b46f628e0dd916a7622c06
Dresmyr <Darkon47@users.noreply.github.com>
2020-03-23 19:36:51 -0400

    Update RandomEnchants.h

--- src/RandomEnchants.h
+++ src/RandomEnchants.h
@@ -2,6 +2,8 @@
 #include "ScriptMgr.h"
 #include "Player.h"
 #include "Configuration/Config.h"
+#include "Chat.h"
+#include "ScriptedGossip.h"
 
 static std::vector<std::vector<std::vector<uint32>>> randomEnchants;
 static std::vector<std::vector<std::vector<uint32>>> getRE() { return randomEnchants; }
